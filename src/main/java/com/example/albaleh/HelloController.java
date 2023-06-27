b1e3ca773e29540471e3d776b923587b8dad95e6
Abbas-Nedal <s12011080@stu.najah.edu>
2023-06-27 23:52:02 +0300

    TEST Add dirctory file

--- src/main/java/com/example/albaleh/HelloController.java
+++ src/main/java/com/example/albaleh/HelloController.java
@@ -3,6 +3,7 @@ package com.example.albaleh;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+
 public class HelloController {
     @FXML
     private Label welcomeText;
---
+++ src/test/java/com/example/albaleh/HelloControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.albaleh;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+
+    @Test
+    void as(){
+
+
+    }
+
+
+}
