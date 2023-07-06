623196f26ac7f63510ea91741ad709b4264c2a78
Abbas-Nedal <s12011080@stu.najah.edu>
2023-07-06 21:58:49 +0300

    create  Initial TestAdmin

--- src/test/java/Runners/Run.java
+++ src/test/java/Runners/Run.java
@@ -6,7 +6,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "")
+@CucumberOptions(features = "use-case", plugin = "html:outputHtml/report.html",monochrome = true,snippets = CucumberOptions.SnippetType.CAMELCASE,
+glue = "Runners")
 public class Run {
+
+
 
 }
